b84e76fc4ea3f210f79399a59d602da3db383b3f
felipewilliam093-design <felipewilliam093@gmail.com>
2025-10-24 22:13:46 -0300

    Atualização do script SQL

--- MedControl/src/main/java/BD/projeto_integrador.sql
+++ MedControl/src/main/java/BD/projeto_integrador.sql
@@ -13,36 +13,56 @@ rua varchar(30) not null,
 numero varchar(5) not null
 );
 
+select * from laboratorio_fornecedor;
+
 create table lista_medicamento(
 codigo int primary key auto_increment,
+cnpj_lab varchar(30),
 custo_unit double not null,
 valor_unit double not null,
-descricao varchar(50) not null
+descricao varchar(50) not null,
+foreign key (cnpj_lab) references laboratorio_fornecedor (cnpj)
 );
+
+select * from lista_medicamento;
 
 create table compra( 
 num_nf_entrada int primary key,
+cnpj_lab varchar(30),
 data_entrada varchar(10) not null,
 valor_total double not null,
 custo_total double not null,
 total_nota double not null,
 forma_pagamento varchar(10),
-data_ult_compra date not null
+data_ult_compra date not null,
+foreign key (cnpj_lab) references laboratorio_fornecedor (cnpj)
 );
+
+select * from compra;
 
 create table item_compra(
 codigo int primary key auto_increment,
+num_nf_entrada_compra int,
 custo_unit double not null,
 valor_unit double not null,
-descricao varchar(50) not null
+descricao varchar(50) not null,
+foreign key (num_nf_entrada_compra) references compra (num_nf_entrada)
 );
+
+select * from item_compra;
 
 create table medicamento(
 codigo int primary key auto_increment,
+num_nf_entrada_compra int,
+num_nf_entrada_venda int,
 custo_unit double not null,
 valor_unit double not null,
-descricao varchar(50) not null
+descricao varchar(50) not null,
+foreign key (num_nf_entrada_compra) references compra (num_nf_entrada),	
+foreign key (num_nf_entrada_venda) references venda (nmr_nf_entrada)
 );
+
+select * from medicamento;
 
 create table drogaria(
 cnpj varchar(30) primary key,
@@ -55,72 +75,35 @@ rua varchar(30) not null,
 numero varchar(5) not null
 );
 
+select * from drogaria;
+
 create table item_venda(
 codigo int primary key auto_increment,
+num_nf_entrada_venda int,
 custo_unit double not null,
 valor_unit double not null,
-descricao varchar(50) not null
+descricao varchar(50) not null,
+foreign key (num_nf_entrada_venda) references venda (nmr_nf_entrada)
 );
+
+select * from item_venda;
 
 create table venda(
 nmr_nf_entrada int primary key,
+cnpj_drogaria varchar(30),
 data_entrega date not null,
 valor_total double not null,
 custo_total double not null,
 qtde int not null,
 total_nota double not null,
 forma_pagamento varchar(10) not null,
-data_ult_compra date not null
+data_ult_compra date not null,
+foreign key (cnpj_drogaria) references drogaria (cnpj)
 );
 
-create table fornece_medicamento(
-cnpj varchar(30),
-codigo int auto_increment,
-foreign key (cnpj) references laboratorio_fornecedor(cnpj),
-foreign key (codigo) references lista_medicamento(codigo)
-);
+select * from venda;
 
-create table fornecedor_compra(
-cnpj varchar(30),
-num_nf_entrada int,
-foreign key (cnpj) references laboratorio_fornecedor(cnpj),
-foreign key (num_nf_entrada) references compra(num_nf_entrada)
-);
 
-create table compra_itemcompra(
-num_nf_entrada int,
-codigo int auto_increment,
-foreign key (num_nf_entrada) references compra(num_nf_entrada),
-foreign key (codigo) references item_compra(codigo)
-);
-
-create table compra_medicamento(
-num_nf_entrada int,
-codigo int auto_increment,
-foreign key (num_nf_entrada) references compra(num_nf_entrada),
-foreign key (codigo) references medicamento(codigo)
-);
-
-create table venda_medicamento(
-nmr_nf_entrada int,
-codigo int auto_increment,
-foreign key (nmr_nf_entrada) references venda(nmr_nf_entrada),
-foreign key (codigo) references medicamento(codigo)
-);
-
-create table venda_drogaria(
-nmr_nf_entrada int,
-cnpj varchar(30),
-foreign key (nmr_nf_entrada) references venda(nmr_nf_entrada),
-foreign key (cnpj) references drogaria(cnpj)
-);
-
-create table venda_itemvenda(
-nmr_nf_entrada int,
-codigo int auto_increment,
-foreign key (nmr_nf_entrada) references venda(nmr_nf_entrada),
-foreign key (codigo) references item_venda(codigo)
-);
 
 
 
