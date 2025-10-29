f6a2592bbe0253fc52382139fa8669b31a365cb9
felipewilliam093-design <felipewilliam093@gmail.com>
2025-10-28 22:23:07 -0300

    Atualizações
    Implementação do ProdutoTableModel e ProdutoDAO

--- MedControl/src/main/java/Model/ProdutoTableModel.java
+++ MedControl/src/main/java/Model/ProdutoTableModel.java
@@ -4,10 +4,37 @@
  */
 package Model;
 
+import Objetos.Cadastro;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
  * @author william.flima4
  */
-public class ProdutoTableModel {
+public class ProdutoTableModel extends AbstractTableModel {
+    private List<Cadastro> dados = new ArrayList<>();
+    private String[] colunas = {"IE", "Nome", "CEP", "Estado", "Cidade", "Bairro", "Rua", "Numero"};
+    
+    @Override
+    public String getColumnName(int column) {
+        return colunas[column];
+    }
+
+    @Override
+    public int getRowCount() {
+        return dados.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
